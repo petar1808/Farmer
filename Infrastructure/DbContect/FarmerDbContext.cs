5c2fb2f4554524d0e6bf47afb44759b3fcb1c8c9
petar1808 <p.yorgov@outlook.com>
2022-03-09 21:13:39 +0200

    Initial commit

---
+++ Infrastructure/DbContect/FarmerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DbContect
+{
+    public class FarmerDbContext : DbContext
+    {
+        public FarmerDbContext()
+            : base()
+        {
+        }
+    }
+}
---
+++ Infrastructure/InfrastructureConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.DbContect;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public static class InfrastructureConfiguration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDataBase(configuration);
+            return services;
+        }
+
+        private static IServiceCollection AddDataBase(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<FarmerDbContext>(opt =>
+            {
+                var connectionString = configuration
+                    .GetSection("ConnectionStrings:DefaultConnection").Value;
+                opt.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
