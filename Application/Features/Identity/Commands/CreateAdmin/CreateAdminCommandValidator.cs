da784f1d00aeab5aaf20d29a9b1911ed6426bf22
petar1808 <p.yorgov@outlook.com>
2023-02-21 13:31:16 +0200

    Fix fluent validation

--- Application/Features/Identity/Commands/CreateAdmin/CreateAdminCommandValidator.cs
+++ Application/Features/Identity/Commands/CreateAdmin/CreateAdminCommandValidator.cs
@@ -11,6 +11,14 @@ namespace Application.Features.Identity.Commands.CreateAdmin
             this.RuleFor(x => x.UserEmail)
                 .EmailAddress()
                 .WithMessage("Имейлът е невалиден");
+
+            this.RuleFor(x => x.FirstName)
+                .Length(2, 50)
+                .WithMessage("Името трябва да е между 2 и 50 символа");
+
+            this.RuleFor(x => x.LastName)
+                .Length(2, 50)
+                .WithMessage("Фамилията трябва да е между 2 и 50 символа");
         }
     }
 }
--- Application/Features/Identity/Commands/CreateUser/CreateUserCommandValidator.cs
+++ Application/Features/Identity/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -9,6 +9,14 @@ namespace Application.Features.Identity.Commands.CreateUser
             this.RuleFor(x => x.UserEmail)
                 .EmailAddress()
                 .WithMessage("Имейлът е невалиден");
+
+            this.RuleFor(x => x.FirstName)
+                .Length(2, 50)
+                .WithMessage("Името трябва да е между 2 и 50 символа");
+
+            this.RuleFor(x => x.LastName)
+                .Length(2, 50)
+                .WithMessage("Фамилията трябва да е между 2 и 50 символа");
         }
     }
 }
