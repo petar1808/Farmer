a232cf441c3c3c38c737ab67d690ab5b2913b359
Petar Yorgov <90971315+petar1808@users.noreply.github.com>
2024-09-10 17:04:36 +0300

    Main layout improvements (#35)

--- WebUI/Pages/Identity/Login/Login.cs
+++ WebUI/Pages/Identity/Login/Login.cs
@@ -42,7 +42,7 @@ namespace WebUI.Pages.Identity.Login
 
         public async Task OnResetPassword()
         {
-            await DialogService.OpenAsync<ForgotPasswordPage>("Reset password");
+            await DialogService.OpenAsync<ForgotPassword>("Забравена парола");
         }
     }
 }
