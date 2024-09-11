c544df4c055475bca0d5192d5bcf9dc211a31f5e
petar <Milen@DESKTOP-32FFL9V>
2024-09-11 15:55:26 +0300

    AuthenticationService: Add check if the identity token is expired

--- WebUI/Services/AuthenticationService.cs
+++ WebUI/Services/AuthenticationService.cs
@@ -1,6 +1,7 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq.Dynamic.Core.Tokenizer;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 
@@ -18,16 +19,28 @@ namespace WebUI.Services
         }
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var token = await _localStorage.GetItemAsStringAsync("token");
+            var tokenString = await _localStorage.GetItemAsStringAsync("token");
 
             var identity = new ClaimsIdentity();
             _http.DefaultRequestHeaders.Authorization = null;
 
-            if (!string.IsNullOrEmpty(token))
+            if (!string.IsNullOrEmpty(tokenString))
             {
-                identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
-                _http.DefaultRequestHeaders.Authorization =
-                    new AuthenticationHeaderValue("Bearer", token.Replace("\"", ""));
+                var handler = new JwtSecurityTokenHandler();
+                var jwtToken = handler.ReadJwtToken(tokenString);            
+
+                if (jwtToken.ValidTo > DateTime.UtcNow)
+                {
+                    // Token is valid
+                    identity = new ClaimsIdentity(jwtToken.Claims, "jwt");
+                    _http.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("Bearer", tokenString.Replace("\"", ""));
+                }
+                else
+                {
+                    // Token has expired, clear it from local storage
+                    await _localStorage.RemoveItemAsync("token");
+                }
             }
 
             var user = new ClaimsPrincipal(identity);
@@ -36,14 +49,6 @@ namespace WebUI.Services
             NotifyAuthenticationStateChanged(Task.FromResult(state));
 
             return state;
-        }
-
-        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
-        {
-            var handler = new JwtSecurityTokenHandler();
-            var token = handler.ReadJwtToken(jwt);
-
-            return token.Claims;
         }
     }
 }
