5c42367320d2a227ff32e1ae355c297b0d6665e0
petar <Milen@DESKTOP-32FFL9V>
2024-11-02 12:13:30 +0200

    Refactor some dialogs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -16,59 +16,61 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.File("logs/startup-log.txt", rollingInterval: RollingInterval.Day)
     .CreateBootstrapLogger();  
 
+
+Log.Information("Starting application");
+
+var MyAllowSpecificOrigins = "_Origins";
+
+builder.Services.AddCors(p => p.AddPolicy(MyAllowSpecificOrigins, policy =>
+{
+    policy.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<FilterInvalidModelStateResponse>(int.MinValue);
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication(builder.Configuration);
+
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+    configuration.ReadFrom.Services(services);
+});
+
+if (builder.Configuration.GetValue<string?>("ApplicationInsights:InstrumentationKey") != null)
+{
+    builder.Services.AddApplicationInsightsTelemetry();
+}
+
+builder.Services.AddTransient<ICurrentUserService, CurrentUserService>();
+builder.Services.AddValidatorsFromAssembly(Assembly.Load(nameof(Application)));
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Local")
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection()
+    .UseRouting()
+    .SeedIdentityUsers(builder.Configuration)
+    .UseCors(MyAllowSpecificOrigins)
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseMiddleware<ValidationExceptionHandlerMiddleware>()
+    .UseEndpoints(endpoints => endpoints.MapControllers());
+
 try
 {
-    Log.Information("Starting application");
-
-    var MyAllowSpecificOrigins = "_Origins";
-
-    builder.Services.AddCors(p => p.AddPolicy(MyAllowSpecificOrigins, policy =>
-    {
-        policy.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-    }));
-
-    builder.Services.AddControllers(options =>
-    {
-        options.Filters.Add<FilterInvalidModelStateResponse>(int.MinValue);
-    });
-
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    builder.Services.AddInfrastructure(builder.Configuration);
-    builder.Services.AddApplication(builder.Configuration);
-
-    builder.Host.UseSerilog((context, services, configuration) =>
-    {
-        configuration.ReadFrom.Configuration(context.Configuration);
-        configuration.ReadFrom.Services(services);
-    });
-
-    if (builder.Configuration.GetValue<string?>("ApplicationInsights:InstrumentationKey") != null)
-    {
-        builder.Services.AddApplicationInsightsTelemetry();
-    }
-
-    builder.Services.AddTransient<ICurrentUserService, CurrentUserService>();
-    builder.Services.AddValidatorsFromAssembly(Assembly.Load(nameof(Application)));
-
-    var app = builder.Build();
-
-    if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Local")
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection()
-        .UseRouting()
-        .SeedIdentityUsers(builder.Configuration)
-        .UseCors(MyAllowSpecificOrigins)
-        .UseAuthentication()
-        .UseAuthorization()
-        .UseMiddleware<ValidationExceptionHandlerMiddleware>()
-        .UseEndpoints(endpoints => endpoints.MapControllers());
-
     app.Run();
 }
 catch (Exception ex)
