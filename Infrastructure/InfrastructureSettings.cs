5639a759b894486ea5d7aba52e99c117c55cce3a
Petar Yorgov <90971315+petar1808@users.noreply.github.com>
2024-11-08 21:19:13 +0200

    Add BlobStorageService and SqlLiteBackupHostedService (#56)

--- Infrastructure/InfrastructureSettings.cs
+++ Infrastructure/InfrastructureSettings.cs
@@ -7,6 +7,8 @@
         public DatabaseProvider DatabaseProvider { get; set; }
 
         public bool EnableSensitiveDataLogging { get; set; }
+
+        public bool BackupEnabled { get; set; }
     }
 
     public enum DatabaseProvider
