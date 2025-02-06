fbe0af048372a3a04ab19e07f453eb95ac871aef
Petar Yorgov <p.yorgov@outlook.com>
2025-02-06 02:01:50 +0200

    Add version

---
+++ src/API/WebApi/Controllers/VersionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/version")]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetVersion()
+        {
+            var version = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                          ?? "Unknown";
+            return Ok(new { version });
+        }
+    }
+}
